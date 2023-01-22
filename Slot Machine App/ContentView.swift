fd9e0d26a83a738503b412d9c250327f8ce9243e
Carlos <cnoramb1@my.centennialcollege.ca>
2023-01-22 16:43:37 -0500

    Check winnings, Won, Bet Entry, Button Reset & Exit

--- Slot Machine App/ContentView.swift
+++ Slot Machine App/ContentView.swift
@@ -4,15 +4,25 @@
 //
 //  Created by Carlos Norambuena on 2023-01-22.
 //
+//  File name: Slot Machine APP
+//  Author's name: Carlos Norambuena Perez
+//  Student ID: 301265667
+//  Date: 2023-01-22
+//  App Description: Assignment 1 - Slot Machine Part 1
+
+//  Version of Xcode: Version 14.2 (14C18)
 
 import SwiftUI
 
 struct ContentView: View {
     
     private var symbols = ["Clubs", "Spades", "Hearts"]
-    private var betAmount = 5
+    private var betAmount = 3
     @State private var numbers = [2, 1, 0]
-    @State private var credits = 500
+    @State private var credits = 150
+    @State private var currentJackpot = 150000
+    @State private var betEntry = 1
+
     
     var body: some View {
         ZStack {
@@ -23,64 +33,108 @@ struct ContentView: View {
     
             
             VStack {
-                               
-                Spacer()
-                
-                // Title
-                HStack {
-                    Image(systemName: "moon.stars.fill")
-                        .foregroundColor(.cyan)
+                              
+                Group {
+                    Spacer()
                     
-                    Text("Slot Machine App")
-                        .fontWeight(.bold)
-                    
-                    Image(systemName: "moon.stars.fill")
-                        .foregroundColor(.cyan)
-                }.scaleEffect(1.5)
-                
-                
-                Spacer()
-                
-                // Credit Counter
-                Text("Credits: " + String(credits))
-                    .foregroundColor(.black)
-                    .padding(.all, 10)
-                    .background(Color.white.opacity(0.5))
-                    .cornerRadius(20)
-                
-                
-                Spacer()
-                
-                // Cards
-                HStack {
+                    // Title
+                    HStack {
+                        Image(systemName: "moon.stars.fill")
+                            .foregroundColor(.cyan)
+                        
+                        Text("Slot Machine App")
+                            .fontWeight(.bold)
+                        
+                        Image(systemName: "moon.stars.fill")
+                            .foregroundColor(.cyan)
+                    }.scaleEffect(1.5)
                     
                     Spacer()
 
-                    Image(symbols[numbers[0]])
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.white.opacity(0.5))
-                        .cornerRadius(20)
+                    // Current Jackpot
+                    Text("Current Jackpot: " + String(currentJackpot))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.all, 10)
+                        .background(Color.black.opacity(0.5))
+                        .cornerRadius(50)
                     
-                    Image(symbols[numbers[1]])
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(Color.white.opacity(0.5))
-                        .cornerRadius(20)
+                    Spacer()
+                }
+                                                
+                Group {
+                    // Cards
+                    HStack {
+                        
+                        Spacer()
+
+                        Image(symbols[numbers[0]])
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.white.opacity(0.5))
+                            .cornerRadius(20)
+                        
+                        Image(symbols[numbers[1]])
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.white.opacity(0.5))
+                            .cornerRadius(20)
+                        
+                        Image(symbols[numbers[2]])
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.white.opacity(0.5))
+                            .cornerRadius(20)
+                        
+                        Spacer()
+                        
+                    }
                     
-                    Image(symbols[numbers[2]])
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
+                    Spacer()
+
+                    // Credit Counter
+                    Text("Credits: " + String(credits))
+                        .foregroundColor(.black)
+                        .padding(.all, 10)
                         .background(Color.white.opacity(0.5))
                         .cornerRadius(20)
                     
                     Spacer()
                     
+                    Button(action: {
+                        
+                        if self.betEntry == 1 {
+                            self.betEntry = 5
+
+                        } else if self.betEntry == 5 {
+                            self.betEntry = 10
+
+                        } else if self.betEntry == 10 {
+                            self.betEntry = 15
+
+                        } else {
+                            self.betEntry = 1
+                            
+                        }
+                        
+                    }) {
+                        Text("Increase Bet")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(.all, 5)
+                            .padding([.leading, .trailing], 30)
+                            .background(Color.pink)
+                            .cornerRadius(50)
+                    }
+                    // Bet Entry
+                    Text("Bet Entry: " + String(betEntry))
+                    
                 }
-                
+
+                                
                 Spacer()
                 
-                // Button
+                // Button Spin
                 Button(action: {
                     
                     // Change the images
@@ -89,10 +143,18 @@ struct ContentView: View {
                     self.numbers[2] = Int.random(in: 0...self.symbols.count - 1)
 
                     // Check winnings
-                    
+                    if self.numbers[0] == self.numbers[1] &&
+                        self.numbers[1] == self.numbers[2] {
+                        
+                        // WON
+                        self.credits += betAmount * 10
+                        
+                    } else {
+                        self.credits -= betAmount
+                    }
                     
                 }) {
-                    Text("Spin")
+                    Text("SPIN")
                         .fontWeight(.bold)
                         .foregroundColor(.white)
                         .padding(.all, 10)
@@ -102,6 +164,42 @@ struct ContentView: View {
                 }
                 
                 Spacer()
+                    
+                // Button Reset & Exit
+                HStack {
+                    
+                    Spacer()
+                    
+
+                    // Button Reset
+                    Button(action: {
+                        
+                    }) {
+                        Text("RESET")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(.all, 10)
+                            .padding([.leading, .trailing], 30)
+                            .background(Color.pink)
+                            .cornerRadius(50)
+                    }
+                    
+                    // Button Exit
+                    Button(action: {
+                        
+                    }) {
+                        Text("EXIT")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(.all, 10)
+                            .padding([.leading, .trailing], 30)
+                            .background(Color.pink)
+                            .cornerRadius(50)
+                    }
+                    
+                    Spacer()
+
+                }
                 
             }
             
